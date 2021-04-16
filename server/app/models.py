fabd2835a5787f8f58ba0675633e1a01fa0e7266
juan gonzalez <jcgonzalez.t3@gmail.com>
2021-04-15 22:38:16 -0700

    update

--- server/app/models.py
+++ server/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class File(models.Model):
+    file_name = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='media/')
--- server/app/views.py
+++ server/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-
+from .models import*
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'all_files': File.objects.all()
+    }
+    return render(request, 'index.html', context)
